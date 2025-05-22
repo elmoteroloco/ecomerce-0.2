2690b0ecaef6c9ebaf5ec04b2dfd674d86edcd1a
elmoteroloco <elmoteroloco@gmail.com>
2025-05-21 22:30:12 -0300

    responsividad del navbar/offcanvas mejoras de comportamientos del offcanvas y correcciones de estilo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import Container from "react-bootstrap/Container"
 import NavBS from "react-bootstrap/Nav"
@@ -6,32 +7,77 @@ import Badge from "react-bootstrap/Badge"
 import Offcanvas from "react-bootstrap/Offcanvas"
 
 function Nav({ productosCarrito, usuarioLogueado, adminLogueado }) {
+    const [showOffcanvas, setShowOffcanvas] = useState(false)
+    const handleShow = () => setShowOffcanvas(true)
+    const handleClose = () => setShowOffcanvas(false)
     return (
         <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
             <Container>
-                <Navbar.Toggle aria-controls="offcanvas-navbar" />
+                <Navbar.Toggle
+                    aria-controls="offcanvas-navbar"
+                    onClick={handleShow}
+                    className="d-lg-none"
+                />
+                <NavBS className="navbar-nav justify-content-evenly d-none d-lg-flex w-100">
+                    {/* Links del menú principal (solo desktop) */}
+                    <NavBS.Link as={Link} to="/">
+                        Inicio
+                    </NavBS.Link>
+                    <NavBS.Link as={Link} to="/productos">
+                        Productos
+                    </NavBS.Link>
+                    <NavBS.Link as={Link} to="/nosotros">
+                        Nosotros
+                    </NavBS.Link>
+                    <NavBS.Link as={Link} to="/contacto">
+                        Contacto
+                    </NavBS.Link>
+                    <NavBS.Link as={Link} to="/carrito">
+                        Carrito{" "}
+                        {productosCarrito.length > 0 && (
+                            <Badge bg="success" pill>
+                                {productosCarrito.length}
+                            </Badge>
+                        )}
+                    </NavBS.Link>
+                    <NavBS.Link as={Link} to="/login">
+                        {usuarioLogueado
+                            ? "Cerrar sesión Usuario"
+                            : !usuarioLogueado && !adminLogueado
+                            ? "Login"
+                            : null}
+                    </NavBS.Link>
+                    {adminLogueado && (
+                        <NavBS.Link as={Link} to="/login">
+                            Cerrar sesión Admin
+                        </NavBS.Link>
+                    )}
+                </NavBS>
                 <Navbar.Offcanvas
                     id="offcanvas-navbar"
                     aria-labelledby="offcanvas-navbar-label"
-                    placement="end">
+                    placement="end"
+                    show={showOffcanvas}
+                    onHide={handleClose}>
                     <Offcanvas.Header closeButton>
                         <Offcanvas.Title id="offcanvas-navbar-label">Menú</Offcanvas.Title>
                     </Offcanvas.Header>
                     <Offcanvas.Body>
-                        <NavBS className="w-100 justify-content-around">
-                            <NavBS.Link as={Link} to="/">
+                        <NavBS className="justify-content-evenly d-lg-none">
+                            {/* Links del menú Offcanvas (solo mobile) */}
+                            <NavBS.Link as={Link} to="/" onClick={handleClose}>
                                 Inicio
                             </NavBS.Link>
-                            <NavBS.Link as={Link} to="/productos">
+                            <NavBS.Link as={Link} to="/productos" onClick={handleClose}>
                                 Productos
                             </NavBS.Link>
-                            <NavBS.Link as={Link} to="/nosotros">
+                            <NavBS.Link as={Link} to="/nosotros" onClick={handleClose}>
                                 Nosotros
                             </NavBS.Link>
-                            <NavBS.Link as={Link} to="/contacto">
+                            <NavBS.Link as={Link} to="/contacto" onClick={handleClose}>
                                 Contacto
                             </NavBS.Link>
-                            <NavBS.Link as={Link} to="/carrito">
+                            <NavBS.Link as={Link} to="/carrito" onClick={handleClose}>
                                 Carrito{" "}
                                 {productosCarrito.length > 0 && (
                                     <Badge bg="success" pill>
@@ -39,7 +85,7 @@ function Nav({ productosCarrito, usuarioLogueado, adminLogueado }) {
                                     </Badge>
                                 )}
                             </NavBS.Link>
-                            <NavBS.Link as={Link} to="/login">
+                            <NavBS.Link as={Link} to="/login" onClick={handleClose}>
                                 {usuarioLogueado
                                     ? "Cerrar sesión Usuario"
                                     : !usuarioLogueado && !adminLogueado
@@ -47,7 +93,7 @@ function Nav({ productosCarrito, usuarioLogueado, adminLogueado }) {
                                     : null}
                             </NavBS.Link>
                             {adminLogueado && (
-                                <NavBS.Link as={Link} to="/login">
+                                <NavBS.Link as={Link} to="/login" onClick={handleClose}>
                                     Cerrar sesión Admin
                                 </NavBS.Link>
                             )}
